b6dbbdd92377ac23ec41792d79866dd8660137f0
ApocalyVec <s-vector.lee@hotmail.com>
2019-07-13 07:22:50 -0400

    update experiment with dist

--- experiment.py
+++ experiment.py
@@ -10,33 +10,100 @@ dist = 'eu'
 res_list = generate_source(fn, feature_num=5)
 
 # initialize the spark context
-conf = SparkConf().\
-    setMaster("local[*]").\
-    setAppName("Genex").set('spark.driver.memory', '64G').\
+conf = SparkConf(). \
+    setMaster("local[*]"). \
+    setAppName("Genex").set('spark.driver.memory', '64G'). \
     set('spark.driver.maxResultSize', '64G')
 
 sc = SparkContext(conf=conf)
 
-# Cluster of length 211 to 274
-c_211_274_start_time = datetime.datetime.now()
-c_211_274 = do_gcluster(input_list=res_list, loi=[211, 274], sc=sc, similarity_threshold=0.1, del_data=True, is_collect=True, dist_type=dist,
-                data_slices=1024, log_level=1)
-c_211_274_end_time = datetime.datetime.now()
+# Clustering with Euclidean distance ########################################################################
+eu_dist = 'eu'
+
+c_211_274_eu_start_time = datetime.datetime.now()
+c_211_274_eu = do_gcluster(input_list=res_list, loi=[211, 274], sc=sc, similarity_threshold=0.1, del_data=True,
+                           is_collect=True, dist_type=eu_dist,
+                           data_slices=1024, log_level=1)
+c_211_274_eu_end_time = datetime.datetime.now()
 
 # Cluster of length 147 to 210
-c_147_210_start_time = datetime.datetime.now()
-c_147_210 = do_gcluster(input_list=res_list, loi=[147, 210], sc=sc, similarity_threshold=0.1, del_data=True, is_collect=True, dist_type=dist,
-                data_slices=1024, log_level=1)
-c_147_210_end_time = datetime.datetime.now()
+c_147_210_eu_start_time = datetime.datetime.now()
+c_147_210_eu = do_gcluster(input_list=res_list, loi=[147, 210], sc=sc, similarity_threshold=0.1, del_data=True,
+                           is_collect=True, dist_type=eu_dist,
+                           data_slices=1024, log_level=1)
+c_147_210_eu_end_time = datetime.datetime.now()
 
 # Cluster of length 83 to 146
-c_83_146_start_time = datetime.datetime.now()
-c_83_146 = do_gcluster(input_list=res_list, loi=[83, 146], sc=sc, similarity_threshold=0.1, del_data=True, is_collect=True, dist_type=dist,
-                data_slices=1024, log_level=1)
-c_83_146_end_time = datetime.datetime.now()
+c_83_146_eu_start_time = datetime.datetime.now()
+c_83_146_eu = do_gcluster(input_list=res_list, loi=[83, 146], sc=sc, similarity_threshold=0.1, del_data=True,
+                          is_collect=True, dist_type=eu_dist,
+                          data_slices=1024, log_level=1)
+c_83_146_eu_end_time = datetime.datetime.now()
 
 # Cluster of length 19 to 82
-c_19_82_start_time = datetime.datetime.now()
-c_19_82 = do_gcluster(input_list=res_list, loi=[19, 82], sc=sc, similarity_threshold=0.1, del_data=True, is_collect=True, dist_type=dist,
-                data_slices=1024, log_level=1)
-c_19_82_end_time = datetime.datetime.now()
+c_19_82_eu_start_time = datetime.datetime.now()
+c_19_82_eu = do_gcluster(input_list=res_list, loi=[19, 82], sc=sc, similarity_threshold=0.1, del_data=True,
+                         is_collect=True, dist_type=eu_dist,
+                         data_slices=1024, log_level=1)
+c_19_82_eu_end_time = datetime.datetime.now()
+
+# Clustering with Manhattan distance ########################################################################
+ma_dist = 'ma'
+
+c_211_274_ma_start_time = datetime.datetime.now()
+c_211_274_ma = do_gcluster(input_list=res_list, loi=[211, 274], sc=sc, similarity_threshold=0.1, del_data=True,
+                           is_collect=True, dist_type=ma_dist,
+                           data_slices=1024, log_level=1)
+c_211_274_ma_end_time = datetime.datetime.now()
+
+# Cluster of length 147 to 210
+c_147_210_ma_start_time = datetime.datetime.now()
+c_147_210_ma = do_gcluster(input_list=res_list, loi=[147, 210], sc=sc, similarity_threshold=0.1, del_data=True,
+                           is_collect=True, dist_type=ma_dist,
+                           data_slices=1024, log_level=1)
+c_147_210_ma_end_time = datetime.datetime.now()
+
+# Cluster of length 83 to 146
+c_83_146_ma_start_time = datetime.datetime.now()
+c_83_146_ma = do_gcluster(input_list=res_list, loi=[83, 146], sc=sc, similarity_threshold=0.1, del_data=True,
+                          is_collect=True, dist_type=ma_dist,
+                          data_slices=1024, log_level=1)
+c_83_146_ma_end_time = datetime.datetime.now()
+
+# Cluster of length 19 to 82
+c_19_82_ma_start_time = datetime.datetime.now()
+c_19_82_ma = do_gcluster(input_list=res_list, loi=[19, 82], sc=sc, similarity_threshold=0.1, del_data=True,
+                         is_collect=True, dist_type=ma_dist,
+                         data_slices=1024, log_level=1)
+c_19_82_ma_end_time = datetime.datetime.now()
+
+# Clustering with Chebyshev distance ########################################################################
+ch_dist = 'ch'
+
+c_211_274_ch_start_time = datetime.datetime.now()
+c_211_274_ch = do_gcluster(input_list=res_list, loi=[211, 274], sc=sc, similarity_threshold=0.1, del_data=True,
+                           is_collect=True, dist_type=ch_dist,
+                           data_slices=1024, log_level=1)
+c_211_274_ch_end_time = datetime.datetime.now()
+
+# Cluster of length 147 to 210
+c_147_210_ch_start_time = datetime.datetime.now()
+c_147_210_ch = do_gcluster(input_list=res_list, loi=[147, 210], sc=sc, similarity_threshold=0.1, del_data=True,
+                           is_collect=True, dist_type=ch_dist,
+                           data_slices=1024, log_level=1)
+c_147_210_ch_end_time = datetime.datetime.now()
+
+# Cluster of length 83 to 146
+c_83_146_ch_start_time = datetime.datetime.now()
+c_83_146_ch = do_gcluster(input_list=res_list, loi=[83, 146], sc=sc, similarity_threshold=0.1, del_data=True,
+                          is_collect=True, dist_type=ch_dist,
+                          data_slices=1024, log_level=1)
+c_83_146_ch_end_time = datetime.datetime.now()
+
+# Cluster of length 19 to 82
+c_19_82_ch_start_time = datetime.datetime.now()
+c_19_82_ch = do_gcluster(input_list=res_list, loi=[19, 82], sc=sc, similarity_threshold=0.1, del_data=True,
+                         is_collect=True, dist_type=ch_dist,
+                         data_slices=1024, log_level=1)
+c_19_82_ch_end_time = datetime.datetime.now()
+
