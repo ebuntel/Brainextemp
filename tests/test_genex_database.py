1eb962784ac0aa4a166b47ebc23be94fc9f59733
YuncM98 <lorraine-c98@outlook.com>
2020-02-03 03:57:53 -0500

    Updated unit tests and modified circleCI config file

--- tests/test_genex_database.py
+++ tests/test_genex_database.py
@@ -4,27 +4,18 @@ import sys
 
 from psutil import virtual_memory
 from pyspark import SparkConf, SparkContext
-from genex.database import genex_database as gxdb
+from genex.database import genexengine as gxdb
 from genex.cluster import _cluster_groups
 
 
 class TestGenex_database:
     num_cores = multiprocessing.cpu_count()
-    # mem = virtual_memory()
-    # i = mem.total
-    conf = SparkConf(). \
-        setMaster("local[" + str(num_cores) + "]"). \
-        setAppName("Genex").set('spark.driver.memory', '12G'). \
-        set('spark.driver.maxResultSize', '12G')
-
-    sc = SparkContext(conf=conf)
 
     def test_python_version(self):
         assert sys.version_info[0] == 3
 
     def test_from_csv(self):
         def _check_unique(x: list):
-            pass
             seen = set()
             return not any(i in seen or seen.add(i) for i in x)
 
@@ -34,7 +25,7 @@ class TestGenex_database:
         df = pd.read_csv(data_file, header=None, skiprows=1, usecols=[0])
         df_raw_id_num = len(df.groupby(list(df.columns[:])))
 
-        test_db = gxdb.from_csv(file_name=data_file, sc=self.sc, feature_num=feature_num, is_header=False)
+        test_db = gxdb.from_csv(file_name=data_file, feature_num=feature_num, num_worker=self.num_cores, is_header=False)
         test_db_id_ls = [x[0] for x in test_db.data]
 
         assert _check_unique(test_db_id_ls)
@@ -46,7 +37,7 @@ class TestGenex_database:
 
         df = pd.read_csv(data_file, index_col=[x for x in range(feature_num)])
 
-        test_db = gxdb.from_csv(file_name=data_file, sc=self.sc, feature_num=feature_num)
+        test_db = gxdb.from_csv(file_name=data_file, feature_num=feature_num, num_worker=self.num_cores)
         test_db_id_ls = [x[0] for x in test_db.data]
 
         assert _check_unique(test_db_id_ls)
@@ -58,24 +49,24 @@ class TestGenex_database:
         path = '../experiments/results/test_db'
 
         # Before executing build method
-        db = gxdb.from_csv(data_file, sc=self.sc, feature_num=feature_num, is_header=False)
+        db = gxdb.from_csv(data_file, feature_num=feature_num, num_worker=self.num_cores, is_header=False)
         db_attribute = db.conf
         db_data = db.data
         db.save(path=path)
 
-        db_after_save = gxdb.from_db(self.sc, path)
+        db_after_save = gxdb.from_db(path=path, num_worker=self.num_cores)
         assert db_attribute == db_after_save.conf
         assert db_data == db_after_save.data
         del db
 
         # After executing build method
-        db = gxdb.from_csv(data_file, sc=self.sc, feature_num=feature_num, is_header=False)
+        db = gxdb.from_csv(data_file, feature_num=feature_num, num_worker=self.num_cores, is_header=False)
         db.build(similarity_threshold=0.1, loi=slice(90, 95))
         db_attribute = db.conf
         db_data = db.data
         db.save(path=path)
 
-        db_after_save = gxdb.from_db(self.sc, path)
+        db_after_save = gxdb.from_db(path=path, num_worker=self.num_cores)
         assert db_attribute == db_after_save.conf
         assert db_data == db_after_save.data
 
@@ -84,7 +75,7 @@ class TestGenex_database:
         feature_num = 1
 
         # Checking numbers of subsequences before grouping
-        mydb = gxdb.from_csv(data_file, sc=self.sc, feature_num=feature_num, is_header=False)
+        mydb = gxdb.from_csv(data_file, feature_num=feature_num, num_worker=self.num_cores, is_header=False)
         mydb_data_test = mydb.data_normalized[:22]
 
         input_rdd = self.sc.parallelize(mydb_data_test, numSlices=self.sc.defaultParallelism)
