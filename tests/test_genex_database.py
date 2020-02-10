5435ccf5580cee2154061dc0cfc5304ff94b2b9b
LamoYc8 <lorraine-c98@outlook.com>
2020-02-10 05:54:16 -0500

    Updated unittest file and added wrapper method

--- tests/test_genex_database.py
+++ tests/test_genex_database.py
@@ -1,11 +1,9 @@
 import multiprocessing
 import pandas as pd
 import sys
-
-from psutil import virtual_memory
-from pyspark import SparkConf, SparkContext
+import pytest as pt
 from genex.database import genexengine as gxdb
-from genex.cluster import _cluster_groups
+from genex.utils import gxe_utils as gutils
 
 
 class TestGenex_database:
@@ -14,87 +12,104 @@ class TestGenex_database:
     def test_python_version(self):
         assert sys.version_info[0] == 3
 
+    def test_load(self):
+        # related path vs absolute path
+        data_file = '../genex/experiments/data_original/Gun_Point_TRAIN.csv'
+        # Missing a default parameter while loading a data file
+        with pt.raises(Exception) as e:
+            gutils.load(data_file, num_worker=self.num_cores)
+        assert 'provide a valid feature number' in str(e.value)
+
+        fake_path = '../genex/fake_path'
+        # Loading a dataset from a fake path
+        with pt.raises(ValueError) as e:
+            gutils.load(fake_path, num_worker=self.num_cores)
+        assert 'Not a valid file name or directory path' in str(e.value)
+
     def test_from_csv(self):
+        # TODO read_csv header=None and skiprows=1
         def _check_unique(x: list):
             seen = set()
             return not any(i in seen or seen.add(i) for i in x)
 
-        data_file = '../experiments/data/Gun_Point_TRAIN.csv'
+        data_file = '../genex/experiments/data/Gun_Point_TRAIN.csv'
         feature_num = 1
 
         df = pd.read_csv(data_file, header=None, skiprows=1, usecols=[0])
-        df_raw_id_num = len(df.groupby(list(df.columns[:])))
 
-        test_db = gxdb.from_csv(file_name=data_file, feature_num=feature_num, num_worker=self.num_cores, is_header=False)
-        test_db_id_ls = [x[0] for x in test_db.data]
+        test_db = gutils.from_csv(file_name=data_file, feature_num=feature_num, num_worker=self.num_cores, use_spark=True)
+        test_db_id_ls = [x[0] for x in test_db.data_original]
 
         assert _check_unique(test_db_id_ls)
         assert len(df) == len(test_db_id_ls)
+        test_db.stop()
+        del test_db
 
         # Running test for another dataset including column header
-        data_file = '../experiments/data/ItalyPower.csv'
+        data_file = '../genex/experiments/data/ItalyPower.csv'
         feature_num = 2
 
         df = pd.read_csv(data_file, index_col=[x for x in range(feature_num)])
 
-        test_db = gxdb.from_csv(file_name=data_file, feature_num=feature_num, num_worker=self.num_cores)
-        test_db_id_ls = [x[0] for x in test_db.data]
+        test_db = gutils.from_csv(file_name=data_file, feature_num=feature_num, num_worker=self.num_cores, use_spark=True)
+        test_db_id_ls = [x[0] for x in test_db.data_original]
 
         assert _check_unique(test_db_id_ls)
         assert len(df) == len(test_db_id_ls)
 
     def test_from_db(self):
-        data_file = '../experiments/data/Gun_Point_TRAIN.csv'
-        feature_num = 1
-        path = '../experiments/results/test_db'
+        data_file = '../genex/experiments/data_original/ItalyPower.csv'
+        feature_num = 2
+        path = '../experiments/unittest/test_db'
 
         # Before executing build method
-        db = gxdb.from_csv(data_file, feature_num=feature_num, num_worker=self.num_cores, is_header=False)
-        db_attribute = db.conf
-        db_data = db.data
+        db = gutils.load(file_or_path=data_file, num_worker=self.num_cores, feature_num=feature_num)
+        db_attributes = db.conf
+        db_data_original = db.data_original
         db.save(path=path)
-
-        db_after_save = gxdb.from_db(path=path, num_worker=self.num_cores)
-        assert db_attribute == db_after_save.conf
-        assert db_data == db_after_save.data
+        db.stop()
         del db
 
+        db_after_save = gutils.load(file_or_path=path, num_worker=self.num_cores)
+
+        assert db_attributes == db_after_save.conf
+        assert db_data_original == db_after_save.data_original
+        db_after_save.stop()
+        del db_after_save
+
         # After executing build method
-        db = gxdb.from_csv(data_file, feature_num=feature_num, num_worker=self.num_cores, is_header=False)
-        db.build(similarity_threshold=0.1, loi=slice(90, 95))
-        db_attribute = db.conf
-        db_data = db.data
+        db = gutils.load(file_or_path=data_file, num_worker=self.num_cores, feature_num=feature_num)
+        db_attributes = db.conf
+        db_data_original = db.data_original
         db.save(path=path)
+        db.stop()
+        del db
 
-        db_after_save = gxdb.from_db(path=path, num_worker=self.num_cores)
-        assert db_attribute == db_after_save.conf
-        assert db_data == db_after_save.data
+        db_after_save = gutils.load(file_or_path=path, num_worker=self.num_cores)
 
-    def test_build_cluster(self):
-        data_file = '../experiments/data/Gun_Point_TRAIN.csv'
-        feature_num = 1
+        assert db_attributes == db_after_save.conf
+        assert db_data_original == db_after_save.data_original
+        db_after_save.stop()
+
+    def test_build(self):
+        data_file = '../genex/experiments/data_original/ItalyPower.csv'
+        feature_num = 2
 
         # Checking numbers of subsequences before grouping
-        mydb = gxdb.from_csv(data_file, feature_num=feature_num, num_worker=self.num_cores, is_header=False)
-        mydb_data_test = mydb.data_normalized[:22]
+        test_db = gutils.from_csv(data_file, feature_num=feature_num, num_worker=12,
+                                  use_spark=True, _rows_to_consider=15)
 
-        input_rdd = self.sc.parallelize(mydb_data_test, numSlices=self.sc.defaultParallelism)
-        group_rdd = input_rdd.mapPartitions(
-            lambda x: gxdb._group_time_series(time_series=x, start=89, end=94), preservesPartitioning=True).cache()
-        sub_sq_count = group_rdd.map(lambda x: len(x[1])).collect()
+        test_db.build(st=0.05, loi=(test_db.get_max_seq_len()-5, test_db.get_max_seq_len()))
 
-        sub_sq_num = 0
-        for num in sub_sq_count:
-            sub_sq_num += num
+        seq_num_group = test_db.get_num_subsequences()
+        d = test_db.cluster_meta_dict
+        count = 0
 
-        print('Number of subsequence before grouping operation')
+        for v in d.values():
+            for num in v.values():
+                count += num
 
-        # number of sub_sq after grouping
-        # cluster_rdd = group_rdd.mapPartitions(lambda x: _cluster_groups(
-        #     groups=x, st=0.01, dist_type='eu', log_level=1)).cache()
-        #
-        # result = cluster_rdd.collect()
-        print()
+        assert seq_num_group == count
 
     def test_query(self):
         pass
