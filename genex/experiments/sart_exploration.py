49d895994a129e857b4fe9602b9c3f5377bcb938
ApocalyVec <s-vector.lee@hotmail.com>
2020-04-21 19:37:41 -0400

    revise ucr experiment script

--- genex/experiments/sart_exploration.py
+++ genex/experiments/sart_exploration.py
@@ -11,7 +11,7 @@ events = 'true answer', 'bCurrent Item: nonTarget',
 
 results = ['target incorrect']
 
-data_all = 'D:/data/sart/101-SART-June2018-AS_(all).csv'
+data_all = '/home/apocalyvec/data/sart/101-SART-June2018-AS_(all).csv'
 
-gxe = from_csv(data_all, feature_num=5, num_worker=32, use_spark=True, driver_mem=24, max_result_mem=24)
+gxe = from_csv(data_all, feature_num=5, header=None, num_worker=32, use_spark=True, driver_mem=24, max_result_mem=24)
 gxe.build(st=cluster_st, loi=[int(sampling_rate)])  # cluster only sequences that are longer than 1 second
