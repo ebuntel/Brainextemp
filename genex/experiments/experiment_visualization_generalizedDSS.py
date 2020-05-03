6e338d309beabf252e911b2a8ece1603926b6d61
ApocalyVec <zli12@wpi.edu>
2020-05-02 23:12:59 -0400

    make visualization script for generalized DSS

--- genex/experiments/experiment_visualization_generalizedDSS.py
+++ genex/experiments/experiment_visualization_generalizedDSS.py
@@ -5,6 +5,10 @@ import matplotlib
 import pandas as pd
 import numpy as np
 
+
+def min_max_normalize(l):
+    return (l - np.min(l)) / (np.max(l) - np.min(l))
+
 if __name__ == '__main__':
     font = {'family': 'DejaVu Sans',
             'weight': 'bold',
@@ -14,12 +18,12 @@ if __name__ == '__main__':
     # date = 'Jan-30-2020-12-N-UseSpark-R1-noOptFastDTW_numSample400'
     # notes = 'UseSpark-R1-noOptFastDTW_numSample400'
     notes = 'UCR_Small[0-50000]'
-    root = 'results/ucr_experiment/DSS/Apr-29-2020-15-N-UCR_test_eu_soi_0-to-50000'
+    root = 'results/ucr_experiment/GeneralizedDSS/Apr-29-2020-15-N-UCR_test_soi_0-to-50000'
     file_list = os.listdir(root)
     file_list = [os.path.join(root, x) for x in file_list]
 
-    # dt_dict = {'eu': 'Euclidean', 'ma': 'Manhattan', 'ch': 'Chebyshev'}
-    dt_dict = {'eu': 'Euclidean'}
+    dt_dict = {'eu': 'Euclidean', 'ma': 'Manhattan', 'ch': 'Chebyshev'}
+    # dt_dict = {'eu': 'Euclidean'}
 
     fig_name = 'Query Performance across Dataset Sizes\n'
     k_to_look = [1, 5, 15]
@@ -87,13 +91,20 @@ if __name__ == '__main__':
             pass
 
         note = '\n Each dot represents on dataset'
+        prep_line = np.linspace(min(bin_size), max(bin_size), 100)
 
         # Plot the Cluster Time
         fig, ax = plt.subplots()
         fig.set_size_inches(15, 8)
         plt.title('Cluster time across Data Size for Distance Type: ' + dt_dict[dt] + note)
-        plt.scatter(bin_size, bin_paa_prep_time, c='blue', label='PAA Preparation Time')
+        # plt.scatter(bin_size, bin_paa_prep_time, c='blue', label='PAA Preparation Time')
+
+        model = np.poly1d(np.polyfit(bin_size, bin_gx_prep_time, 3))
+        plt.plot(prep_line, model(prep_line), label='Genex Cluster Time Fitted', c='cyan')
         plt.scatter(bin_size, bin_gx_prep_time, c='cyan', label='Genex Cluster Time', marker='x')
+
+        model = np.poly1d(np.polyfit(bin_size, bin_dss_prep_time, 3))
+        plt.plot(prep_line, model(prep_line), label='DSS Cluster Time Fitted', c='green')
         plt.scatter(bin_size, bin_dss_prep_time, c='green', label='DSS Cluster Time', marker='x')
         plt.ylabel('Time (second)')
         plt.xlabel('Time series length (number of data points)')
@@ -104,10 +115,17 @@ if __name__ == '__main__':
         # Plot the Query Time
         fig, ax = plt.subplots()
         fig.set_size_inches(15, 8)
+
         plt.title('Query Time across Data Size for Distance Type: ' + dt_dict[dt] + note)
-        plt.scatter(bin_size, bin_qbf_time, c='red', label='Brute Force Query Time')
-        plt.scatter(bin_size, bin_qpaa_time, c='orange', label='PAA Query Time')
+        # plt.scatter(bin_size, bin_qbf_time, c='red', label='Brute Force Query Time')
+        # plt.scatter(bin_size, bin_qpaa_time, c='orange', label='PAA Query Time')
+
+        model = np.poly1d(np.polyfit(bin_size, bin_qgx_time, 3))  # change the Y vector in this line
+        plt.plot(prep_line, model(prep_line), label='Genex Query Time Fitted', c='cyan')
         plt.scatter(bin_size, bin_qgx_time, c='blue', label='Genex Query Time')
+
+        model = np.poly1d(np.polyfit(bin_size, bin_qdss_time, 3))  # change the Y vector in this line
+        plt.plot(prep_line, model(prep_line), label='DSS Cluster Time Fitted', c='green')
         plt.scatter(bin_size, bin_qdss_time, c='green', label='DSS Query Time')
         plt.ylabel('Time (second)')
         plt.xlabel('Time series length (number of data points)')
@@ -118,9 +136,19 @@ if __name__ == '__main__':
         # Plot the accuracy
         fig, ax = plt.subplots()
         fig.set_size_inches(15, 8)
+        # if dt == 'ch':  # normalize chebyshev error
+        #     bin_qpaa_error = min_max_normalize(bin_qpaa_error)
+        #     bin_qgx_error = min_max_normalize(bin_qgx_error)
+        #     bin_qdss_error = min_max_normalize(bin_qdss_error)
+
         plt.title('Normalized Error across Data Size for Distance Type: ' + dt_dict[dt] + note)
-        plt.scatter(bin_size, bin_qpaa_error, c='orange', label='PAA Query Error')
+        # plt.scatter(bin_size, bin_qpaa_error, c='orange', label='PAA Query Error')
+        model = np.poly1d(np.polyfit(bin_size, bin_qgx_error, 3))  # change the Y vector in this line
+        plt.plot(prep_line, model(prep_line), label='Genex Query Error Fitted', c='cyan')
         plt.scatter(bin_size, bin_qgx_error, c='blue', label='Genex Query Error')
+
+        model = np.poly1d(np.polyfit(bin_size, bin_qdss_error, 3))  # change the Y vector in this line
+        plt.plot(prep_line, model(prep_line), label='DSS Query Time Fitted', c='green')
         plt.scatter(bin_size, bin_qdss_error, c='green', label='DSS Query Error')
         plt.ylabel('Normalized Error')
         plt.xlabel('Data size (number of data points)')
