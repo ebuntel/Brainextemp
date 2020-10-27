ce44406c5bd8c7a185e9a79238d68286643449ea
Ebuntel <buntel.ellery@gmail.com>
2020-10-27 09:24:50 -0400

    Lowered ram settings

--- brainex/experiments/testbed_ucr.py
+++ brainex/experiments/testbed_ucr.py
@@ -82,8 +82,8 @@ if __name__ == "__main__":
         'best_ks': [1, 5, 15]
     }
     mp_args = {'num_worker': 72,
-               'driver_mem': 72,
-               'max_result_mem': 72}
+               'driver_mem': 70,
+               'max_result_mem': 70}
 
     # End of Config Parameters, Experiment starts here ################################################################
     # at the moment, there are three different experiments
